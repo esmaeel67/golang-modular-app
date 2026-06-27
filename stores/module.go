20c7bce1449f767a8df206a00ee6189b194ae6ca
esmaeel67 <esmaeel.babaee@gmail.com>
2026-06-27 13:39:11 +0330

    feat(store-module): implement store module and import to main modules

---
+++ stores/module.go
@@ -0,0 +1,30 @@
+package stores
+
+import (
+	"context"
+
+	"github.com/esmaeel67/golang-modular-app/internal/monolith"
+	"github.com/esmaeel67/golang-modular-app/stores/internal/application"
+	"github.com/esmaeel67/golang-modular-app/stores/internal/grpc"
+	"github.com/esmaeel67/golang-modular-app/stores/internal/logging"
+	"github.com/esmaeel67/golang-modular-app/stores/internal/postgres"
+)
+
+type Module struct {
+}
+
+func (m *Module) Startup(ctx context.Context, mono monolith.Monolith) error {
+	stores := postgres.NewStoreRepository("stores", mono.DB())
+	participatingStores := postgres.NewParticipatingStoryRepository("stores", mono.DB())
+	products := postgres.NewProductRepository("products", mono.DB())
+
+	var app application.App
+	app = application.New(stores, participatingStores, products)
+	app = logging.LogApplicationAccess(app, mono.Logger())
+
+	if err := grpc.RegisterService(ctx, app, mono.RPC()); err != nil {
+		return err
+	}
+
+	return nil
+}
