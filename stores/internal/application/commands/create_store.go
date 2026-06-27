8cfe8a2ea35c88ab84b0df101037f01d7c882c66
esmaeel67 <esmaeel.babaee@gmail.com>
2026-06-27 13:33:19 +0330

    feat(create-store-command): implement create store command

---
+++ stores/internal/application/commands/create_store.go
@@ -0,0 +1,32 @@
+package commands
+
+import (
+	"context"
+
+	"github.com/esmaeel67/golang-modular-app/stores/internal/domain"
+)
+
+type (
+	CreateStore struct {
+		ID       string
+		Name     string
+		Location string
+	}
+	CreateStoreHandler struct {
+		stores domain.StoreRepository
+	}
+)
+
+func NewCreateStoreHandler(stores domain.StoreRepository) CreateStoreHandler {
+	return CreateStoreHandler{stores: stores}
+}
+
+func (h CreateStoreHandler) CreateStore(ctx context.Context, cmd CreateStore) error {
+	store, err := domain.CreateStore(cmd.ID, cmd.Name, cmd.Location)
+	if err != nil {
+		return err
+	}
+
+	err = h.stores.Save(ctx, store)
+	return err
+}
