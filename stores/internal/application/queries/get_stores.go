7ebd201f061c4fbaffcb0520000a80b50c0ced12
esmaeel67 <esmaeel.babaee@gmail.com>
2026-06-27 13:34:48 +0330

    feat(get-stores-query): implement get store query

---
+++ stores/internal/application/queries/get_stores.go
@@ -0,0 +1,24 @@
+package queries
+
+import (
+	"context"
+
+	"github.com/esmaeel67/golang-modular-app/stores/internal/domain"
+)
+
+type GetStores struct {
+}
+
+type GetStoresHandler struct {
+	stores domain.StoreRepository
+}
+
+func NewGetStoresHandler(stores domain.StoreRepository) GetStoresHandler {
+	return GetStoresHandler{
+		stores: stores,
+	}
+}
+
+func (h GetStoresHandler) GetStores(ctx context.Context, _ GetStores) ([]*domain.Store, error) {
+	return h.stores.FindAll(ctx)
+}
