29d0b2745e94b158d399d4342611f63d986f8682
esmaeel67 <esmaeel.babaee@gmail.com>
2026-06-27 17:44:57 +0330

    feat(add-product): implement add product command and handler

---
+++ stores/internal/application/commands/add_product.go
@@ -0,0 +1,42 @@
+package commands
+
+import (
+	"context"
+
+	"github.com/esmaeel67/golang-modular-app/stores/internal/domain"
+	"github.com/stackus/errors"
+)
+
+type AddProduct struct {
+	ID          string
+	StoreID     string
+	Name        string
+	Description string
+	SKU         string
+	Price       float64
+}
+
+type AddProductHandler struct {
+	stores   domain.StoreRepository
+	products domain.ProductRepository
+}
+
+func NewAddProductHandler(stores domain.StoreRepository, products domain.ProductRepository) AddProductHandler {
+	return AddProductHandler{stores: stores, products: products}
+}
+
+func (h AddProductHandler) AddProduct(ctx context.Context, cmd AddProduct) error {
+
+	_, err := h.stores.Find(ctx, cmd.StoreID)
+	if err != nil {
+		return errors.Wrap(err, "error adding product(store not found)")
+	}
+	product, err := domain.CreateProduct(cmd.ID, cmd.StoreID, cmd.Name, cmd.Description, cmd.SKU, cmd.Price)
+
+	if err != nil {
+		return errors.Wrap(err, "error adding product")
+	}
+
+	return errors.Wrap(h.Products.AddProduct(ctx, product), "error adding product")
+
+}
