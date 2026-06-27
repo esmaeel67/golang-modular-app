5187cc42db88d7efa8656810979322d853ef93a2
esmaeel67 <esmaeel.babaee@gmail.com>
2026-06-27 12:50:36 +0330

    feat(main): initial web and rpc server structure

--- cmd/mallbots/main.go
+++ cmd/mallbots/main.go
@@ -1,8 +1,21 @@
 package main
 
 import (
+	"database/sql"
 	"fmt"
+	"net/http"
 	"os"
+
+	"github.com/esmaeel67/golang-modular-app/internal/config"
+	"github.com/esmaeel67/golang-modular-app/internal/logger"
+	"github.com/esmaeel67/golang-modular-app/internal/monolith"
+	"github.com/esmaeel67/golang-modular-app/internal/rpc"
+	"github.com/esmaeel67/golang-modular-app/internal/waiter"
+	"github.com/esmaeel67/golang-modular-app/internal/web"
+	"github.com/go-chi/chi/v5"
+	_ "github.com/jackc/pgx/v5/stdlib"
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/reflection"
 )
 
 func main() {
@@ -14,5 +27,54 @@ func main() {
 
 func run() error {
 
-	return nil
+	var cfg config.AppConfig
+	cfg, err := config.InitConfig()
+	if err != nil {
+		return err
+	}
+	m := app{cfg: cfg}
+	// setup database connection // cfg.PG.Conn
+	m.db, err = sql.Open("pgx", cfg.PG.Conn)
+	// m.db, err = sql.Open("pgx", "postgres://postgres:admin@localhost:5432/mallbots?sslmode=disable")
+	if err != nil {
+		return err
+	}
+	defer func(db *sql.DB) {
+		err := db.Close()
+		if err != nil {
+			return
+		}
+	}(m.db)
+
+	// logger config
+	m.logger = logger.NewLogger(&cfg)
+
+	m.rpc = initRpc(cfg.Rpc)
+	m.mux = initMux(cfg.Web)
+	m.waiter = waiter.New(waiter.CatchSignals())
+
+	m.modules = []monolith.Module{}
+
+	if err = m.startupModules(); err != nil {
+		return err
+	}
+
+	m.mux.Mount("/", http.FileServer(http.FS(web.WebUI)))
+
+	fmt.Println("started app")
+	defer fmt.Println("stopped app")
+
+	m.waiter.Add(m.waitForWeb, m.waitForRPC)
+
+	return m.waiter.Wait()
+}
+
+func initRpc(_ rpc.RpcConfig) *grpc.Server {
+	server := grpc.NewServer()
+	reflection.Register(server)
+	return server
+}
+
+func initMux(_ web.WebConfig) *chi.Mux {
+	return chi.NewMux()
 }
