0ef3984aaf2ca128986809666bb1e8b939535ab8
esmaeel67 <esmaeel.babaee@gmail.com>
2026-06-29 20:42:28 +0330

    feat(basket-domain): add basket dependency repository to domain and rpc client and update basket module application params

---
+++ baskets/internal/grpc/product_repository.go
@@ -0,0 +1,38 @@
+package grpc
+
+import (
+	"context"
+
+	"github.com/esmaeel67/golang-modular-app/baskets/internal/domain"
+	"github.com/esmaeel67/golang-modular-app/stores/storespb"
+	"github.com/stackus/errors"
+	"google.golang.org/grpc"
+)
+
+type ProductRepository struct {
+	client storespb.StoresServiceClient
+}
+
+var _ domain.ProductRepository = (*ProductRepository)(nil)
+
+func NewProductRepository(conn *grpc.ClientConn) ProductRepository {
+	return ProductRepository{client: storespb.NewStoresServiceClient(conn)}
+}
+
+func (r ProductRepository) Find(ctx context.Context, productID string) (*domain.Product, error) {
+	resp, err := r.client.GetProduct(ctx, &storespb.GetProductRequest{Id: productID})
+	if err != nil {
+		return nil, errors.Wrap(err, "requesting product")
+	}
+
+	return r.productToDomain(resp.Product), nil
+}
+
+func (r ProductRepository) productToDomain(product *storespb.Product) *domain.Product {
+	return &domain.Product{
+		ID:      product.GetId(),
+		StoreID: product.GetStoreId(),
+		Name:    product.GetName(),
+		Price:   product.GetPrice(),
+	}
+}
