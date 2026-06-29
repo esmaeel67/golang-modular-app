ff9c0ff6850c9eebbab49ffe63e451aa5baf8ca9
esmaeel67 <esmaeel.babaee@gmail.com>
2026-06-29 10:57:40 +0330

    feat(order-logging): initial and implement application and create order

--- ordering/internal/logging/application.go
+++ ordering/internal/logging/application.go
@@ -1,1 +1,32 @@
 package logging
+
+import (
+	"context"
+
+	"github.com/esmaeel67/golang-modular-app/internal/logger"
+	"github.com/esmaeel67/golang-modular-app/ordering/internal/application"
+	"github.com/esmaeel67/golang-modular-app/ordering/internal/application/commands"
+)
+
+type Application struct {
+	application.App
+	logger logger.Logger
+}
+
+var _ application.App = (*Application)(nil)
+
+func NewApplication(application application.App, logger logger.Logger) Application {
+	return Application{
+		App:    application,
+		logger: logger,
+	}
+}
+
+func (a Application) CreateOrder(ctx context.Context, cmd commands.CreateOrderCommand) (err error) {
+	a.logger.Info(logger.Customers, logger.RegisterCustomer, "--> Ordering.CreateOrder", nil)
+	defer func() {
+		a.logger.Info(logger.Customers, logger.RegisterCustomer, "<-- Ordering.CreateOrder", nil)
+	}()
+
+	return a.App.CreateOrder(ctx, cmd)
+}
