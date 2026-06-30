9bee46e65b551def4c2f50fa9edc1107059ed71f
esmaeel67 <esmaeel.babaee@gmail.com>
2026-06-30 17:32:49 +0330

    feat(order-create-handler): fix and implement create order handler

--- ordering/internal/application/commands/create_order.go
+++ ordering/internal/application/commands/create_order.go
@@ -3,7 +3,9 @@ package commands
 import (
 	"context"
 
+	"github.com/esmaeel67/golang-modular-app/internal/ddd"
 	"github.com/esmaeel67/golang-modular-app/ordering/internal/domain"
+	"github.com/stackus/errors"
 )
 
 type CreateOrderCommand struct {
@@ -14,26 +16,55 @@ type CreateOrderCommand struct {
 }
 
 type CreateOrderHandler struct {
-	orders        domain.OrderRepository
-	customers     domain.CustomerRepository
-	payments      domain.PaymentRepository
-	shopping      domain.ShoppingRepository
-	notifications domain.NotificationRepository
+	orders          domain.OrderRepository
+	customers       domain.CustomerRepository
+	payments        domain.PaymentRepository
+	shopping        domain.ShoppingRepository
+	domainPublisher ddd.EventPublisher
 }
 
 func NewCreateOrderHandler(orders domain.OrderRepository, customers domain.CustomerRepository,
-	payments domain.PaymentRepository, shopping domain.ShoppingRepository, notifications domain.NotificationRepository,
+	payments domain.PaymentRepository, shopping domain.ShoppingRepository, domainPublisher ddd.EventPublisher,
 ) CreateOrderHandler {
 	return CreateOrderHandler{
-		orders:        orders,
-		customers:     customers,
-		payments:      payments,
-		shopping:      shopping,
-		notifications: notifications,
+		orders:          orders,
+		customers:       customers,
+		payments:        payments,
+		shopping:        shopping,
+		domainPublisher: domainPublisher,
 	}
 }
 
 func (h CreateOrderHandler) CreateOrder(ctx context.Context, cmd CreateOrderCommand) error {
+	order, err := domain.CreateOrder(cmd.ID, cmd.CustomerID, cmd.PaymentID, cmd.Items)
+	if err != nil {
+		return errors.Wrap(err, "create order command")
+	}
+
+	// authorizeCustomer
+	if err = h.customers.Authorize(ctx, order.CustomerID); err != nil {
+		return errors.Wrap(err, "order customer authorization")
+	}
+
+	// validatePayment
+	if err = h.payments.Confirm(ctx, order.PaymentID); err != nil {
+		return errors.Wrap(err, "order payment confirmation")
+	}
+
+	// scheduleShopping
+	if order.ShoppingID, err = h.shopping.Create(ctx, order); err != nil {
+		return errors.Wrap(err, "order shopping scheduling")
+	}
+
+	// orderCreation
+	if err = h.orders.Save(ctx, order); err != nil {
+		return errors.Wrap(err, "order creation")
+	}
+
+	// publish domain event
+	if err = h.domainPublisher.Publish(ctx, order.GetEvents()...); err != nil {
+		return err
+	}
 
 	return nil
 }
