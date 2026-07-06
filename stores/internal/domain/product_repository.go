16eee7489bad3460819ce4ae91c1c3b692c8576f
esmaeel67 <esmaeel.babaee@gmail.com>
2026-07-06 12:52:52 +0330

    fix(repository): fixe product and store repository interface

--- stores/internal/domain/product_repository.go
+++ stores/internal/domain/product_repository.go
@@ -3,8 +3,6 @@ package domain
 import "context"
 
 type ProductRepository interface {
-	FindProduct(ctx context.Context, id string) (*Product, error)
-	AddProduct(ctx context.Context, product *Product) error
-	RemoveProduct(ctx context.Context, id string) error
-	GetCatalog(ctx context.Context, storeID string) ([]*Product, error)
+	Load(ctx context.Context, id string) (*Product, error)
+	Save(ctx context.Context, product *Product) error
 }
