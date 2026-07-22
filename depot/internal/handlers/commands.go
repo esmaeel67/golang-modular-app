3cf8c671da34a03882f172928d8f8cb57dbc39e5
esmaeel67 <esmaeel.babaee@gmail.com>
2026-07-22 16:42:03 +0330

    feat(depot-command): implemented command handlers and register command events

---
+++ depot/internal/handlers/commands.go
@@ -0,0 +1,87 @@
+package handlers
+
+import (
+	"context"
+
+	"github.com/esmaeel67/golang-modular-app/depot/depotpb"
+	"github.com/esmaeel67/golang-modular-app/depot/internal/application"
+	"github.com/esmaeel67/golang-modular-app/depot/internal/application/commands"
+	"github.com/esmaeel67/golang-modular-app/internal/am"
+	"github.com/esmaeel67/golang-modular-app/internal/ddd"
+	"github.com/google/uuid"
+)
+
+type commandHandlers struct {
+	app application.App
+}
+
+func NewCommandHandlers(app application.App) ddd.CommandHandler[ddd.Command] {
+	return commandHandlers{
+		app: app,
+	}
+}
+
+func RegisterCommandHandlers(subscriber am.CommandSubscriber, handlers ddd.CommandHandler[ddd.Command]) error {
+	cmdMsgHandler := am.CommandMessageHandlerFunc(func(ctx context.Context, cmdMsg am.IncomingCommandMessage) (ddd.Reply, error) {
+		return handlers.HandleCommand(ctx, cmdMsg)
+	})
+
+	return subscriber.Subscribe(depotpb.CommandChannel, cmdMsgHandler, am.MessageFilter{
+		depotpb.CreateShoppingListCommand,
+		depotpb.CancelShoppingListCommand,
+		depotpb.InitiateShoppingCommand,
+	}, am.GroupName("depot-commands"))
+}
+
+func (h commandHandlers) HandleCommand(ctx context.Context, cmd ddd.Command) (ddd.Reply, error) {
+
+	switch cmd.CommandName() {
+	case depotpb.CreateShoppingListCommand:
+		return h.doCreateShoppingList(ctx, cmd)
+	case depotpb.CancelShoppingListCommand:
+		return h.doCancelShoppingList(ctx, cmd)
+	}
+	return nil, nil
+}
+
+func (h commandHandlers) doCreateShoppingList(ctx context.Context, cmd ddd.Command) (ddd.Reply, error) {
+	payload := cmd.Payload().(*depotpb.CreateShoppingList)
+
+	id := uuid.New().String()
+
+	items := make([]commands.OrderItem, 0, len(payload.GetItems()))
+	for _, item := range payload.GetItems() {
+		items = append(items, commands.OrderItem{
+			StoreID:   item.StoreId,
+			ProductID: item.ProductId,
+			Quantity:  int(item.GetQuantity()),
+		})
+	}
+
+	err := h.app.CreateShoppingList(ctx, commands.CreateShoppingListCommand{
+		ID:      id,
+		OrderID: payload.GetOrderId(),
+		Items:   items,
+	})
+
+	return ddd.NewReply(depotpb.CreatedShoppingListReply, &depotpb.CreatedShoppingList{Id: id}), err
+}
+
+func (h commandHandlers) doCancelShoppingList(ctx context.Context, cmd ddd.Command) (ddd.Reply, error) {
+	payload := cmd.Payload().(*depotpb.CancelShoppingList)
+
+	err := h.app.CancelShoppingList(ctx, commands.CancelShoppingListCommand{
+		ID: payload.GetId(),
+	})
+
+	return nil, err
+}
+
+func (h commandHandlers) doInitiateShopping(ctx context.Context, cmd ddd.Command) (ddd.Reply, error) {
+	payload := cmd.Payload().(*depotpb.InitiateShopping)
+
+	err := h.app.InitiateShopping(ctx, commands.InitiateShoppingCommand{ID: payload.GetId()})
+
+	// returning nil returns a simple Success or Failure reply; err being nil determines which
+	return nil, err
+}
