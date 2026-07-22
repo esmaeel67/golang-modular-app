32080c76171682502f59f8e934153c54ac404a34
esmaeel67 <esmaeel.babaee@gmail.com>
2026-07-22 16:43:23 +0330

    feat(depot): implemented shopping list domain event handlers and implemented shopping list completed

---
+++ depot/internal/handlers/domain_events.go
@@ -0,0 +1,38 @@
+package handlers
+
+import (
+	"context"
+
+	"github.com/esmaeel67/golang-modular-app/depot/internal/domain"
+	"github.com/esmaeel67/golang-modular-app/internal/ddd"
+)
+
+type domainHandlers[T ddd.AggregateEvent] struct {
+	orders domain.OrderRepository
+}
+
+var _ ddd.EventHandler[ddd.AggregateEvent] = (*domainHandlers[ddd.AggregateEvent])(nil)
+
+func NewDomainEventHandlers(orders domain.OrderRepository) ddd.EventHandler[ddd.AggregateEvent] {
+	return domainHandlers[ddd.AggregateEvent]{
+		orders: orders,
+	}
+}
+func RegisterDomainEventHandlers(subscriber ddd.EventSubscriber[ddd.AggregateEvent], handler ddd.EventHandler[ddd.AggregateEvent]) {
+	subscriber.Subscribe(handler, domain.ShoppingListCompletedEvent)
+}
+
+func (h domainHandlers[T]) HandleEvent(ctx context.Context, event T) error {
+
+	switch event.EventName() {
+	case domain.ShoppingListCompletedEvent:
+		return h.onShoppingListCompleted(ctx, event)
+	}
+	return nil
+}
+
+func (h domainHandlers[T]) onShoppingListCompleted(ctx context.Context, event T) error {
+	completed := event.Payload().(*domain.ShoppingListCompleted)
+	return h.orders.Ready(ctx, completed.ShoppingList.OrderID)
+
+}
