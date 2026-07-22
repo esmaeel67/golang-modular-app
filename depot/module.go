740ca71260316b324726bd4648ae5a653c1d92e3
esmaeel67 <esmaeel.babaee@gmail.com>
2026-07-22 18:18:01 +0330

    feat(depot-module): fix depot module and register commands and domain event handles

--- depot/module.go
+++ depot/module.go
@@ -3,21 +3,35 @@ package depot
 import (
 	"context"
 
+	"github.com/esmaeel67/golang-modular-app/depot/depotpb"
 	"github.com/esmaeel67/golang-modular-app/depot/internal/application"
 	"github.com/esmaeel67/golang-modular-app/depot/internal/grpc"
 	"github.com/esmaeel67/golang-modular-app/depot/internal/handlers"
 	"github.com/esmaeel67/golang-modular-app/depot/internal/logging"
 	"github.com/esmaeel67/golang-modular-app/depot/internal/postgres"
+	"github.com/esmaeel67/golang-modular-app/internal/am"
 	"github.com/esmaeel67/golang-modular-app/internal/ddd"
+	"github.com/esmaeel67/golang-modular-app/internal/jetstream"
 	"github.com/esmaeel67/golang-modular-app/internal/monolith"
+	"github.com/esmaeel67/golang-modular-app/internal/registry"
+	"github.com/esmaeel67/golang-modular-app/stores/storespb"
 )
 
 type Module struct {
 }
 
-func (m Module) Startup(ctx context.Context, mono monolith.Monolith) error {
-
+func (m Module) Startup(ctx context.Context, mono monolith.Monolith) (err error) {
 	// setup Driven adapter
+	reg := registry.New()
+	if err = storespb.Registrations(reg); err != nil {
+		return err
+	}
+	if err = depotpb.Registrations(reg); err != nil {
+		return err
+	}
+	stream := jetstream.NewStream(mono.Config().Nats.Stream, mono.JS(), mono.Logger())
+	eventStream := am.NewEventStream(reg, stream)
+	commandStream := am.NewCommandStream(reg, stream)
 	domainDispatcher := ddd.NewEventDispatcher[ddd.AggregateEvent]()
 	shoppingLists := postgres.NewShoppingListRepository("shopping_lists", mono.DB())
 	conn, err := grpc.Dial(ctx, mono.Config().Rpc.Address())
@@ -25,8 +39,8 @@ func (m Module) Startup(ctx context.Context, mono monolith.Monolith) error {
 		return err
 	}
 
-	stores := grpc.NewStoreRepository(conn)
-	products := grpc.NewProductRepository(conn)
+	stores := postgres.NewStoreCacheRepository("stores_cache", mono.DB(), grpc.NewStoreRepository(conn))
+	products := postgres.NewProductCacheRepository("products_cache", mono.DB(), grpc.NewProductRepository(conn))
 	orders := grpc.NewOrderRepository(conn)
 
 	// setup application
@@ -34,9 +48,21 @@ func (m Module) Startup(ctx context.Context, mono monolith.Monolith) error {
 		application.New(shoppingLists, stores, products, domainDispatcher),
 		mono.Logger())
 
-	orderHandlers := logging.LogDomainEventHandlerAccess[ddd.AggregateEvent](
-		application.NewOrderHandlers(orders),
-		"Order",
+	domainEventHandlers := logging.LogEventHandlerAccess[ddd.AggregateEvent](
+		handlers.NewDomainEventHandlers(orders),
+		"DomainEvents",
+		mono.Logger(),
+	)
+
+	integrationHandlers := logging.LogEventHandlerAccess[ddd.Event](
+		handlers.NewIntegrationEventHandlers(stores, products),
+		"IntegrationEvents",
+		mono.Logger(),
+	)
+
+	commandHandlers := logging.LogCommandHandlerAccess[ddd.Command](
+		handlers.NewCommandHandlers(app),
+		"Commands",
 		mono.Logger(),
 	)
 
@@ -47,7 +73,13 @@ func (m Module) Startup(ctx context.Context, mono monolith.Monolith) error {
 		return err
 	}
 
-	handlers.RegisterOrderHandlers(orderHandlers, domainDispatcher)
+	handlers.RegisterDomainEventHandlers(domainDispatcher, domainEventHandlers)
+	if err = handlers.RegisterIntegrationEventHandlers(eventStream, integrationHandlers); err != nil {
+		return err
+	}
+	if err = handlers.RegisterCommandHandlers(commandStream, commandHandlers); err != nil {
+		return err
+	}
 
 	return nil
 }
