932f856a6a5081c4addb3d51bf0880bf5c9090b9
esmaeel67 <esmaeel.babaee@gmail.com>
2026-07-23 13:36:12 +0330

    feat(cosec-module): implemented cosec module

--- cosec/module.go
+++ cosec/module.go
@@ -3,12 +3,81 @@ package cosec
 import (
 	"context"
 
+	"github.com/esmaeel67/golang-modular-app/cosec/internal"
+	"github.com/esmaeel67/golang-modular-app/cosec/internal/handlers"
+	"github.com/esmaeel67/golang-modular-app/cosec/internal/logging"
+	"github.com/esmaeel67/golang-modular-app/cosec/internal/models"
+	"github.com/esmaeel67/golang-modular-app/customers/customerspb"
+	"github.com/esmaeel67/golang-modular-app/depot/depotpb"
+	"github.com/esmaeel67/golang-modular-app/internal/am"
+	"github.com/esmaeel67/golang-modular-app/internal/ddd"
+	"github.com/esmaeel67/golang-modular-app/internal/jetstream"
 	"github.com/esmaeel67/golang-modular-app/internal/monolith"
+	pg "github.com/esmaeel67/golang-modular-app/internal/postgres"
+	"github.com/esmaeel67/golang-modular-app/internal/registry"
+	"github.com/esmaeel67/golang-modular-app/internal/registry/serdes"
+	"github.com/esmaeel67/golang-modular-app/internal/sec"
+	"github.com/esmaeel67/golang-modular-app/ordering/orderingpb"
+	"github.com/esmaeel67/golang-modular-app/payments/paymentspb"
 )
 
 type Module struct{}
 
 func (Module) Startup(ctx context.Context, mono monolith.Monolith) (err error) {
+
+	// setup Driven adapters
+	reg := registry.New()
+	if err = registrations(reg); err != nil {
+		return err
+	}
+	if err = orderingpb.Registrations(reg); err != nil {
+		return err
+	}
+	if err = customerspb.Registrations(reg); err != nil {
+		return err
+	}
+	if err = depotpb.Registrations(reg); err != nil {
+		return err
+	}
+	if err = paymentspb.Registrations(reg); err != nil {
+		return err
+	}
+	stream := jetstream.NewStream(mono.Config().Nats.Stream, mono.JS(), mono.Logger())
+	eventStream := am.NewEventStream(reg, stream)
+	commandStream := am.NewCommandStream(reg, stream)
+	replyStream := am.NewReplyStream(reg, stream)
+
+	sagaStore := pg.NewSagaStore("sagas", mono.DB(), reg)
+	sagaRepo := sec.NewSagaRepository[*models.CreateOrderData](reg, sagaStore)
+
+	// setup application
+	orchestrator := logging.LogReplyHandlerAccess[*models.CreateOrderData](
+		sec.NewOrchestrator[*models.CreateOrderData](internal.NewCreateOrderSaga(), sagaRepo, commandStream),
+		"CreateOrderSaga", mono.Logger(),
+	)
+	integrationEventHandlers := logging.LogEventHandlerAccess[ddd.Event](
+		handlers.NewIntegrationEventHandlers(orchestrator),
+		"IntegrationEvents", mono.Logger(),
+	)
+
+	// setup Driver adapters
+	if err = handlers.RegisterIntegrationEventHandlers(eventStream, integrationEventHandlers); err != nil {
+		return err
+	}
+	if err = handlers.RegisterReplyHandlers(replyStream, orchestrator); err != nil {
+		return err
+	}
+
+	return nil
+}
+
+func registrations(reg registry.Registry) (err error) {
+	serde := serdes.NewJsonSerde(reg)
+
+	// Saga data
+	if err = serde.RegisterKey(internal.CreateOrderSagaName, models.CreateOrderData{}); err != nil {
+		return err
+	}
 
 	return nil
 }
