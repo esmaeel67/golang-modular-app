ff0605fc6f160305287fe06371d05ec8ee66b1a5
esmaeel67 <esmaeel.babaee@gmail.com>
2026-07-24 13:52:09 +0330

    feat(notification-event-logger): implemented event handler logger

---
+++ notifications/internal/logging/event_handlers.go
@@ -0,0 +1,35 @@
+package logging
+
+import (
+	"context"
+	"fmt"
+
+	"github.com/esmaeel67/golang-modular-app/internal/ddd"
+	"github.com/esmaeel67/golang-modular-app/internal/logger"
+)
+
+type EventHandlers[T ddd.Event] struct {
+	ddd.EventHandler[T]
+	label  string
+	logger logger.Logger
+}
+
+var _ ddd.EventHandler[ddd.Event] = (*EventHandlers[ddd.Event])(nil)
+
+func LogEventHandlerAccess[T ddd.Event](handlers ddd.EventHandler[T], label string, logger logger.Logger) EventHandlers[T] {
+	return EventHandlers[T]{
+		EventHandler: handlers,
+		label:        label,
+		logger:       logger,
+	}
+}
+
+func (h EventHandlers[T]) HandleEvent(ctx context.Context, event T) (err error) {
+	messageIn := fmt.Sprintf("--> Notifications.%s.On(%s)", h.label, event.EventName())
+	h.logger.Info(logger.Notifications, logger.HandleEvent, messageIn, nil)
+	defer func() {
+		messageOut := fmt.Sprintf("<-- Notifications.%s.On(%s)", h.label, event.EventName())
+		h.logger.Info(logger.Notifications, logger.HandleEvent, messageOut, nil)
+	}()
+	return h.EventHandler.HandleEvent(ctx, event)
+}
