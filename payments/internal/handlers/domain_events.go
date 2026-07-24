8f6f5a2303d9941ba6fa5d7555cec8b6a3e7c4b6
esmaeel67 <esmaeel.babaee@gmail.com>
2026-07-24 19:15:15 +0330

    feat(payment-domain-event): implemented domain event handles

---
+++ payments/internal/handlers/domain_events.go
@@ -0,0 +1,45 @@
+package handlers
+
+import (
+	"context"
+
+	"github.com/esmaeel67/golang-modular-app/internal/am"
+	"github.com/esmaeel67/golang-modular-app/internal/ddd"
+	"github.com/esmaeel67/golang-modular-app/payments/internal/models"
+	"github.com/esmaeel67/golang-modular-app/payments/paymentspb"
+)
+
+type domainHandlers[T ddd.Event] struct {
+	publisher am.MessagePublisher[ddd.Event]
+}
+
+var _ ddd.EventHandler[ddd.Event] = (*domainHandlers[ddd.Event])(nil)
+
+func NewDomainHandlers(publisher am.MessagePublisher[ddd.Event]) ddd.EventHandler[ddd.Event] {
+	return &domainHandlers[ddd.Event]{
+		publisher: publisher,
+	}
+}
+
+func RegisterDomainEventHandlers(subscriber ddd.EventSubscriber[ddd.Event], handlers ddd.EventHandler[ddd.Event]) {
+	subscriber.Subscribe(handlers, models.InvoicePaidEvent)
+}
+
+func (h domainHandlers[T]) HandleEvent(ctx context.Context, event T) error {
+
+	switch event.EventName() {
+	case models.InvoicePaidEvent:
+		return h.onInvoicePaid(ctx, event)
+	}
+	return nil
+}
+
+func (h domainHandlers[T]) onInvoicePaid(ctx context.Context, event ddd.Event) error {
+	payload := event.Payload().(*models.InvoicePaid)
+
+	return h.publisher.Publish(ctx, paymentspb.InvoiceAggregateChannel, ddd.NewEvent(paymentspb.InvoicePaidEvent, &paymentspb.InvoicePaid{
+		Id:      payload.ID,
+		OrderId: payload.OrderID,
+	}))
+
+}
